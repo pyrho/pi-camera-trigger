c2a88faa4a098fb48da0065cbe6b496b5448a5a7
pyrho <github@25.wf>
2023-10-06 23:13:00 +0000

    fix(cam): camera init refactor

--- camera-controller/src/index.ts
+++ camera-controller/src/index.ts
@@ -1,42 +1,56 @@
 import { Gpio } from 'onoff'
-import gphoto2 from 'gphoto2'
+import gphoto2, { Camera } from 'gphoto2'
 import net from 'net'
-
 import config from './config'
 import { debug, error, log } from './logger'
 
-interface CameraDaemon {
-  takePicture: () => Promise<Buffer>
-}
+const takePicture = (() => {
+  let cameraInstance: Camera | null = null
 
-async function startCameraDaemon (): Promise<CameraDaemon> {
-  return await new Promise((resolve, reject) => {
-    const GPhoto = new gphoto2.GPhoto2()
-    GPhoto.list((listOfCameras) => {
-      const [camera] = listOfCameras
-      if (camera === undefined) return reject(new Error('No cameras found'))
-
-      debug(`Found ${camera.model}, ready.`)
-
-      return resolve({
-        takePicture: async () => await new Promise((resolve, reject) => {
-          debug('Taking picture...')
-          camera.takePicture({ download: true }, (err, data) => {
-            if (err != null) return reject(err)
-            else return resolve(data)
-          })
+  const getCamera = () =>
+    new Promise<Camera>((resolve, reject) => {
+      if (cameraInstance === null) {
+        new gphoto2.GPhoto2().list((cameras) => {
+          const [camera] = cameras
+          if (camera === undefined) return reject(new Error('No cameras found'))
+          debug(`Found ${camera.model}, ready.`)
+          cameraInstance = camera
+          return resolve(cameraInstance)
         })
-      })
+      } else {
+        return resolve(cameraInstance)
+      }
     })
-  })
-}
 
-async function sendPictureToWebhook (imgData: Buffer): Promise<void> {
+    // Get the camera at load time to speed up the first picture
+    getCamera().then(() => debug('Camera initialized at load time'))
+
+  return () =>
+    new Promise<Buffer>((resolve, reject) => {
+      debug('Taking picture...')
+      getCamera().then((camera) =>
+        camera.takePicture({ download: true }, (err, data) => {
+          if (err !== null && err !== undefined) {
+            // In the "normal" case, this will happen
+            // if the camera is disconnected
+            // Setting `cameraInstance` to null will force a reinstantiation
+            // of the camera
+            cameraInstance = null
+            return reject(err)
+          } else {
+            return resolve(data)
+          }
+        }),
+      )
+    })
+})()
+
+async function sendPictureToWebhook(imgData: Buffer): Promise<void> {
   return await new Promise((resolve, reject) => {
     const client = net.createConnection({ ...config.socket }, () => {
       debug('Connected to server')
-      client.on('error', e => reject(e))
-      client.write(imgData, err => {
+      client.on('error', (e) => reject(e))
+      client.write(imgData, (err) => {
         if (err != null) {
           return reject(err)
         } else {
@@ -48,28 +62,28 @@ async function sendPictureToWebhook (imgData: Buffer): Promise<void> {
   })
 }
 
-function setupGpioHook (cameraDaemonInstance: CameraDaemon): void {
-  const button = new Gpio(config.gpio.pin, 'in', 'rising', { debounceTimeout: config.gpio.debounce })
+function setupGpioHook(): void {
+  const button = new Gpio(config.gpio.pin, 'in', 'rising', {
+    debounceTimeout: config.gpio.debounce,
+  })
 
   button.watch((err) => {
     if (err != null) error(`GPIO error: ${err.message}`)
 
     debug('Button pressed')
 
-    cameraDaemonInstance.takePicture()
+    takePicture()
       .then(sendPictureToWebhook)
       .then(() => log('Image posted successfully!'))
       .catch((e) => error('Error downloading the image:', e))
   })
 
   // Cleanup
-  process.on('SIGINT', _ => {
+  process.on('SIGINT', (_) => {
     button.unexport()
   })
 
   log('Daemon running')
 }
 
-startCameraDaemon()
-  .then(setupGpioHook)
-  .catch(error)
+setupGpioHook()
