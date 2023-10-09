47f4bd426e770e0f45391c16a8aad824a15bb028
pyrho <github@25.wf>
2023-10-09 14:12:06 +0000

    feat(back): bed temp notif

--- backend/src/notify.ts
+++ backend/src/notify.ts
@@ -1,8 +1,32 @@
 import { request } from 'urllib'
+import { debug, error, log } from './logger'
+import { StatusData, getPrinterStatus } from './get-printer-status'
 
-export function notify(): Promise<void> {
+export async function notify(message = 'Print done!'): Promise<void> {
   return request(`http://ntfy.sh/${process.env.NTFY_TOPIC}`, {
     method: 'POST',
-    data: 'Print done!',
-  }).then(() =>{})
+    data: message,
+  }).then(() => {
+    bedTempNotification()
+  })
+}
+
+const TARGET_TEMP = 35
+
+async function bedTempNotification() {
+  return new Promise(async (resolve) => {
+    const printerStatus = await getPrinterStatus()
+    const bedTemp = printerStatus?.printer.temp_bed
+    if (bedTemp !== undefined) {
+      const iId = setInterval(() => {
+        if (bedTemp <= TARGET_TEMP) {
+          clearInterval(iId)
+          return resolve(notify(`Bed temp below ${TARGET_TEMP}C`))
+        }
+        log(`Bed temp currently at: ${bedTemp}`)
+      }, 1000 * 60)
+    } else {
+      debug('No bed temp data.')
+    }
+  })
 }
